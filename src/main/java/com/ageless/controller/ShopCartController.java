5aab68d180853fdf7bf396a2d0ea5984f7e63971
HaoYaoguo <ghy769509@163.com>
2018-06-23 10:56:42 +0800

    ghy02

---
+++ src/main/java/com/ageless/controller/ShopCartController.java
@@ -0,0 +1,14 @@
+package com.ageless.controller;
+
+
+import com.ageless.pojo.Area;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ShopCartController {
+    @GetMapping("/udai_shopcart.html")
+    public String udai() {
+        return "udai_shopcart";
+    }
+}
