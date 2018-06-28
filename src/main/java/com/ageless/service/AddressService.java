130a6e6b4e2d0ae9b8facef9f33af91a92a09214
xyzstemr <SU8NAIT@163.com>
2018-06-28 14:12:04 +0800

    Merge branch 'master' of https://github.com/Gitjiangwei/Ageless
    # Conflicts:
    #	pom.xml
    #	src/main/java/com/ageless/controller/AddressController.java
    #	src/main/java/com/ageless/controller/UserController.java
    #	src/main/java/com/ageless/mapper/AddressMapper.java
    #	src/main/java/com/ageless/mapper/UserMapper.java
    #	src/main/java/com/ageless/service/AddressService.java
    #	src/main/java/com/ageless/service/UserService.java
    #	src/main/java/com/ageless/service/impl/AddressServiceImpl.java
    #	src/main/java/com/ageless/service/impl/UserServiceImpl.java
    #	src/main/java/com/ageless/util/MD5.java
    #	src/main/resources/application.properties
    #	src/main/resources/mapper/AddressMapper.xml
    #	src/main/resources/mapper/UserMapper.xml
    #	src/main/resources/static/js/login.js
    #	src/main/resources/static/js/udai_address.js
    #	src/main/resources/templates/login.html
    #	src/main/resources/templates/udai_address.html
    #	src/main/resources/templates/udai_setting.html

--- src/main/java/com/ageless/service/AddressService.java
+++ src/main/java/com/ageless/service/AddressService.java
@@ -33,5 +33,12 @@ public interface AddressService {
      * @param address
      * @return
      */
-    Integer updateAddress(Address address,Long id);
+    Integer updateAddress(Address address);
+
+    /**
+     * 根据id显示地址
+     * @param id
+     * @return
+     */
+    Address selectAddress(Long id);
 }
