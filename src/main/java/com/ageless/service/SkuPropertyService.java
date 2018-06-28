50b188ab5825b4d9a68186f0197cb7f268e1a4b0
zxh <769074061@qq.com>
2018-06-28 11:01:13 +0800

    栋哥组属性

---
+++ src/main/java/com/ageless/service/SkuPropertyService.java
@@ -0,0 +1,12 @@
+package com.ageless.service;
+
+import com.ageless.pojo.SkuProperty;
+
+import java.util.List;
+
+public interface SkuPropertyService {
+
+    public List<SkuProperty> seleAll(int id,String name);
+    public int  shan(int  id);
+    public int shan1(int  id);
+}
