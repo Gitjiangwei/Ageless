84c7a196fbc8ce160bd48f7e99dd6351bce5a38b
dongge <401287775@qq.com>
2018-07-09 16:41:34 +0800

    第五组提交

--- src/main/java/com/ageless/pojo/Notice.java
+++ src/main/java/com/ageless/pojo/Notice.java
@@ -1,9 +1,10 @@
 
         package com.ageless.pojo;
 
-import lombok.Data;
+        import com.alibaba.fastjson.annotation.JSONField;
+        import lombok.Data;
 
-import java.util.Date;
+        import java.util.Date;
 
 @Data
 public class Notice {
@@ -12,9 +13,11 @@ public class Notice {
     private  String title;  //标题
     private  String link;   //链接
     private  String announcementContent;   //公告内容
+    @JSONField(format="yyyy-MM-dd")
     private Date announcementTime; //公告时间
 
     private  Integer typeId;  //类型id
+    @JSONField(format="yyyy-MM-dd")
     private  Date modificationDate;  //修改时间
     private  String typeName;  //标题
 
--- src/main/java/com/ageless/service/NoticeService.java
+++ src/main/java/com/ageless/service/NoticeService.java
@@ -1,10 +1,10 @@
 
         package com.ageless.service;
 
-import com.ageless.pojo.Notice;
-import org.apache.ibatis.annotations.Param;
+        import com.ageless.pojo.NewsType;
+        import com.ageless.pojo.Notice;
 
-import java.util.List;
+        import java.util.List;
 
 public interface NoticeService {
     //查询所有方法
@@ -14,11 +14,23 @@ public interface NoticeService {
     //删除
     public int delete(Integer id);
     //修改
-    public int xiuGai(Integer id,String announcement_content);
+    public int xiuGai(Integer id, String title, Integer typeId, String link, String announcementContent);
     //页面查
     public List<Notice> qianselect( Integer id,  Integer typeId, String typeName) ;
     //id查
     public Notice incha(Integer id) ;
+
+    //后台需要
+    //下拉列表查询
+    public List<NewsType> xiaLa();
+    //根据ID查询
+    public Notice selectId(Integer id);
+    //类型查询所有
+    public  List<NewsType> selectNewsType();
+    //类型删除
+    public int deletNews(Integer nId);
+    //类型新增方法
+    public int insertNews(String typeName);
 
 }
 
